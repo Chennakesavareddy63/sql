e18ab5997610f3066f032ee9577c54beef6f48e7
Chennakesavareddy63 <161718710+Chennakesavareddy63@users.noreply.github.com>
2024-03-01 17:26:00 +0530

    Add files via upload

---
+++ feb_2025.sql
@@ -0,0 +1,14 @@
+
+
+
+CREATE TABLE  mobiles_details  (
+
+    moblie_number INT,
+    mobile_name varchar(50),
+    manufacturing_year INT,
+    mobile_cost INT,
+    
+);
+ALTER TABLE mobiles_details ADD mobile_colour varchar(10)
+
+
